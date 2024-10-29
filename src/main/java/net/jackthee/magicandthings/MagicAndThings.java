f545f447e93b0d8319ec7b8bdfd568b016114d44
BuildTools <unconfigured@null.spigotmc.org>
2024-10-29 16:09:16 -0600

    EEEEEEEEEEEEEEEEE

--- src/main/java/net/jackthee/magicandthings/MagicAndThings.java
+++ src/main/java/net/jackthee/magicandthings/MagicAndThings.java
@@ -2,19 +2,19 @@ package net.jackthee.magicandthings;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.jackthee.magicandthings.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MagicAndThings implements ModInitializer {
 
 
-	public static final String MOD_ID = "magic_and_things";
+	public static final String MOD_ID = "magicandthings";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/jackthee/magicandthings/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.jackthee.magicandthings.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.jackthee.magicandthings.MagicAndThings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item RUBY = RegisterItem("ruby",new Item(new FabricItemSettings()));
+
+    private static void AddItemsToIngredientItemGroup(FabricItemGroupEntries entries){
+        entries.add(RUBY);
+    }
+
+
+    private static Item RegisterItem(String Name,Item item){
+        return Registry.register(Registries.ITEM,new Identifier(MagicAndThings.MOD_ID,name),item);
+    }
+
+    public static void registerModItems(){
+        MagicAndThings.LOGGER.info("RegisteringModItems" + MagicAndThings.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::AddItemsToIngredientItemGroup);
+
+    }
+}
