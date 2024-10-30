816f01501645c7ba66f1f49621d11f9d39bce885
BuildTools <unconfigured@null.spigotmc.org>
2024-10-30 14:23:44 -0600

    EEEEEEEEEEEEEEEEE

--- src/main/java/net/jackthee/magicandthings/MagicAndThingsClient.java
+++ src/main/java/net/jackthee/magicandthings/MagicAndThingsClient.java
@@ -1,10 +1,14 @@
 package net.jackthee.magicandthings;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.jackthee.magicandthings.blocks.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class MagicAndThingsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUBY_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUBY_TRAPDOOR, RenderLayer.getCutout());
     }
 }
